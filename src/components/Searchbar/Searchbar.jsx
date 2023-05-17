e776b98ab8ea413648ed46153983b6ab73125961
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-05-17 22:20:58 +0300

    change Searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { ImSearch } from 'react-icons/im';
@@ -6,48 +6,92 @@ import { toast } from 'react-toastify';
 
 import { Header } from './Searchbar.styled';
 
-export default class Searchbar extends Component {
-  static propTypes = {
-    onFormSubmit: PropTypes.func.isRequired,
+export default function Searchbar({ onFormSubmit }) {
+  const [query, setQuery] = useState('');
+
+  const onInputChange = ({ target: { value } }) => {
+    setQuery(value);
   };
 
-  state = {
-    query: '',
+  const resetForm = () => {
+    setQuery('');
   };
-  onInputChange = ({ target: { value } }) => {
-    this.setState({ query: value });
-  };
-  hanldeFormSubmit = event => {
+
+  const hanldeFormSubmit = event => {
     event.preventDefault();
-    if (this.state.query.trim() === '') {
+    if (query.trim() === '') {
       return toast.error('Please enter a name of collection.');
     }
-    this.props.onFormSubmit(this.state.query);
-    this.resetForm();
+    onFormSubmit(query);
+    resetForm();
   };
-  resetForm = () => {
-    this.setState({ query: '' });
-  };
-  render() {
-    const { query } = this.state;
-    return (
-      <Header className="searchbar">
-        <form className="form" onSubmit={this.hanldeFormSubmit}>
-          <button type="submit" className="buttonSearch">
-            <ImSearch color="grey" />
-          </button>
 
-          <input
-            onChange={this.onInputChange}
-            className="searchForm-input"
-            type="text"
-            value={query}
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </Header>
-    );
-  }
+  return (
+    <Header className="searchbar">
+      <form className="form" onSubmit={hanldeFormSubmit}>
+        <button type="submit" className="buttonSearch">
+          <ImSearch color="grey" />
+        </button>
+
+        <input
+          onChange={onInputChange}
+          className="searchForm-input"
+          type="text"
+          value={query}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </Header>
+  );
 }
+Searchbar.propTypes = {
+  onFormSubmit: PropTypes.func.isRequired,
+};
+
+// export default class OLDSearchbar extends Component {
+//   static propTypes = {
+//     onFormSubmit: PropTypes.func.isRequired,
+//   };
+
+//   state = {
+//     query: '',
+//   };
+//   onInputChange = ({ target: { value } }) => {
+//     this.setState({ query: value });
+//   };
+//   hanldeFormSubmit = event => {
+//     event.preventDefault();
+//     if (this.state.query.trim() === '') {
+//       return toast.error('Please enter a name of collection.');
+//     }
+//     this.props.onFormSubmit(this.state.query);
+//     this.resetForm();
+//   };
+//   resetForm = () => {
+//     this.setState({ query: '' });
+//   };
+//   render() {
+//     const { query } = this.state;
+//     return (
+//       <Header className="searchbar">
+//         <form className="form" onSubmit={this.hanldeFormSubmit}>
+//           <button type="submit" className="buttonSearch">
+//             <ImSearch color="grey" />
+//           </button>
+
+//           <input
+//             onChange={this.onInputChange}
+//             className="searchForm-input"
+//             type="text"
+//             value={query}
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//           />
+//         </form>
+//       </Header>
+//     );
+//   }
+// }
